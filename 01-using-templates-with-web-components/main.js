03bb1f03c07dee8bf644f13e1f9bda84d3b50846
elevalgue <elena.valenzuela.guerrero@gmail.com>
2021-02-01 21:03:56 +0100

    Finish second folder exercise

--- 01-using-templates-with-web-components/main.js
+++ 01-using-templates-with-web-components/main.js
@@ -1,3 +1,11 @@
+
+// This won't appear in your page until you grab a reference to it with Js and then append in to the DOM like in the exxample below. 
+
+let template = document.getElementById('my-paragraph');
+let templateContent = template.content;
+document.body.appendChild(templateContent);
+
+
 // Templates are useful on their own, but they work even better with web components.
 
 customElements.define('my-paragraph',
